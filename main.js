d12cd4e17aa3e7b3f5cc3b799607805e9a3be610
najmiddino <164999098+najmiddino@users.noreply.github.com>
2024-06-20 16:36:58 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,78 @@
+//1
+
+// alert (`5:2=${5/2} qoldiq ${5%2}`)
+
+//2
+
+// let random_num =  Math.floor (Math.random() * 10)
+
+// alert (`${random_num }`)
+
+//3
+
+//  let num = Math.floor(12.510)
+
+//  console.log(num);
+
+//4
+
+// let str = ('MARS IT SCHOOL')
+
+// function a() {
+//     alert (str.length)
+// }
+// a()
+
+//5
+
+//  for(let i = 0;  i< 10 ; i++) {
+//      console.log('mars it school' , i+1);
+//  }
+
+// console.log(num);
+
+//6
+
+//  let harflar = ["a" , "b" , "d" , "e" , "f" , "g" , "h" , "i" , "j" , "k" , "l" , "m" , "n" , "o" , "p" , "q" , "r" , "s" , "t",  "u" , "v" , "x" , "y" , "z" ]
+
+// console.log(harflar[21],harflar[19],harflar[16],harflar[17],harflar[6],harflar[7],harflar[2]);
+
+//7
+
+// let str = prompt('yoshingizni kiriting')
+
+// if (str ===18 ) {
+//     alert (`siz balogat yoshidasiz`)
+// } else if (str --- 18) {
+//     alert (`siz balogot yoshiga yetmagansiz`)
+// }
+
+//8
+// let name = prompt('ismingizni kiriying:')
+
+// function rever(str) {
+//     return str.split().reverse().join();
+// }
+
+// console.log('teskarisi:', rever(name));
+
+
+
+
+//9
+
+
+// let num_1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+// let num_2 = []
+
+
+// for(let i=0; i < num_1.length; i++){
+//     if(num_1[i] % 2 == 0){
+//         num_2.push(num_1[i])
+//     }
+// }
+
+// console.log(num_1, 'boshlanishidagi arre');
+// console.log(num_2, 'juft sonlari');
+
+
